85756b7b76a29b780d5805d14f6d21cbbe45f955
unknown <alzein7@outlook.com>
2021-12-02 14:56:30 +0000

    added pages folder in src folder

---
+++ disney-app/src/pages/character.jsx
@@ -0,0 +1,12 @@
+const Character = (props) => {
+    const { character } = props;
+    console.log(character);
+
+    return ( 
+        <>
+        <td></td>
+        </>
+     );
+};
+ 
+export default Character;
