9118116b60ac8702537d1fac12393b6eded2fb7a
Adam Mussa <adammussa1999@gmail.com>
2021-12-02 16:51:26 +0000

    Home Page created
    needs more design and fitting fixes

--- disney-app/src/App.js
+++ disney-app/src/App.js
@@ -3,22 +3,66 @@ import "./CSS/App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Disney">
       </header>
+      <div class="parallax4"></div>
+      
+      <div class="para">
+        <div class="WelcomePara">
+        <h4>
+          Welcome to the disney Website
+        </h4>
+        <h5>
+          Other Infomation
+        </h5>
+        </div>
+      </div>
+      
+      <div class="parallax"></div>
+
+      <div class="para">
+        <div class="WelcomePara">
+        <h4>
+          Maybe Movies and other stuff here
+        </h4>
+        <h5>
+         and another link here maybe?
+        </h5>
+        </div>
+      </div>
+
+      <div class="parallax3"></div>
+      
+     <div class="para">
+        <div class="WelcomePara">
+        <h4>
+         Loren Impsum stuff
+        </h4>
+        <h5>
+          some more stuff that can be here
+        </h5>
+        </div>
+      </div>
+
+      <div class="parallax2"></div>
+      
+ <div class="para">
+        <div class="WelcomePara">
+        <h4>
+          Contact Info
+        </h4>
+        <h5>
+          If ya Like me 079 me
+        </h5>
+        </div>
+      </div>
     </div>
+
+    
   );
 }
+
+
+export default App;
 
 export default App;
