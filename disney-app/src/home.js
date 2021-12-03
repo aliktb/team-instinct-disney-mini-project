8bc12a7e5fd53f6eceda77f17c2c2c095cf6751e
Adam Mussa <adammussa1999@gmail.com>
2021-12-03 10:03:45 +0000

    Home Page

---
+++ disney-app/src/home.js
@@ -0,0 +1,40 @@
+
+import './css/Home.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Disney">
+
+      
+
+<div sticky="top" id="navbar">
+  <a class="active" href="javascript:void(0)">Home</a>
+  <a href="javascript:void(0)">News</a>
+  <a href="javascript:void(0)">Contact</a>
+        </div>
+           <div class="parallax" ></div>
+      </header>
+
+      <div class="para">
+        <h5>
+         the header
+        </h5>
+        <h2>Team instinct Babyyyyyyyy</h2>
+        </div>
+  
+      
+    
+      </div>
+       
+
+    
+    
+  );
+}
+
+
+
+
+export default App;
