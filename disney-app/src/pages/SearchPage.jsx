dfdbc7944287b184e9e427701cf8262fece25d3c
sean-fernyhough <sean.fernyhough97@gmail.com>
2021-12-03 09:45:49 +0000

    search page functionality completed

--- disney-app/src/pages/SearchPage.jsx
+++ disney-app/src/pages/SearchPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Search from "../components/Search";
+import { Card, CardBody, CardImg, CardTitle, CardGroup } from 'reactstrap'
 
 const SearchPage = () => {
 
@@ -7,11 +8,51 @@ const SearchPage = () => {
 
     if (results.length > 0) {
 
+        const card = results.map((char) => {
+
+            let picUrl = char.imageUrl;
+
+            if (picUrl.indexOf(".png") !== -1) {
+                picUrl = picUrl.substring(0, picUrl.indexOf(".png"));
+                picUrl = picUrl + ".png";
+                console.log(picUrl);
+            }
+            if (picUrl.indexOf(".PNG") !== -1) {
+                picUrl = picUrl.substring(0, picUrl.indexOf(".PNG"));
+                picUrl = picUrl + ".PNG";
+                console.log(picUrl);
+            } else if (picUrl.indexOf(".jpg") !== -1) {
+                picUrl = picUrl.substring(0, picUrl.indexOf(".jpg"));
+                picUrl = picUrl + ".jpg";
+                console.log(picUrl);
+            } else if (picUrl.indexOf(".jpeg") !== -1) {
+                picUrl = picUrl.substring(0, picUrl.indexOf(".jpeg"));
+                picUrl = picUrl + ".jpeg";
+                console.log(picUrl);
+            }
+
+            return (
+                <span>
+                    <Card style={{ maxWidth: "20rem", height: "25rem" }} className="m-2">
+                        <CardImg alt={char.name} src={picUrl} top width="100%" />
+                        <CardBody>
+                            <CardTitle tag="h5">{char.name}</CardTitle>
+                        </CardBody>
+                    </Card>
+                </span>
+            );
+        });
+
         return (
             <div>
-
+                <Search setResults={setResults} />
+                <div>
+                    <CardGroup>
+                        <>{card}</>
+                    </CardGroup>
+                </div>
             </div>
-        )
+        );
     } else {
 
         return (
